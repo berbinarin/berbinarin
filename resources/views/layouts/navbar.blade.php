6732f5924c1c2c1a587fedc195d537390c750464
Firdausi Baiq Ardana <firdaardana@student.ub.ac.id>
2023-12-26 11:21:52 +0700

    menu home at mobile view

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -32,8 +32,63 @@
                 With Us</a>
         </div>
 
-        <div class="flex md:hidden items-center justify-center p-4">
+        <div class="flex md:hidden items-center justify-center p-4" onclick="Open()">
             <i class='bx bx-menu text-[35px] text-disabled'></i>
+            
         </div>
+        <div class="sidebar text-center justify-center items-center fixed top-0 bottom-0 w-screen lg-right-0 p-2 overflow-y-auto right-full bg-white">
+            <div class="p-2.5 mt-3 flex rounded-md px-4 duration-300 cursor-pointer text-gray-700">
+                <div class="flex ml-auto p-8">
+                    <i class="bx bx-x align-items-end text-[35px] text-disabled" onclick="Open()"></i>
+                </div>  
+            </div>
+            <div class="m-auto text-lg flex items-center justify-center p-4 duration-300 cursor-pointer hover:text-primary text-gray-700" onclick="navigateToPage('home')">
+                Home
+            </div>
+            <div class="m-auto text-lg flex items-center justify-center p-4 duration-300 cursor-pointer hover:text-primary text-gray-700" onclick="navigateToPage('about-us')">
+                Tentang Kami
+            </div>
+            <div class="m-auto text-lg flex items-center justify-center p-4 duration-300 cursor-pointer hover:text-primary text-gray-700" onclick="navigateToPage('products')">
+                Produk Berbinar
+            </div>
+            <div class="m-auto text-lg flex items-center justify-center p-4 duration-300 cursor-pointer hover:text-primary text-gray-700" onclick="navigateToPage('contact')">
+                Kontak
+            </div>
+            <div class="m-auto text-lg flex items-center justify-center p-4 duration-300 cursor-pointer hover:text-primary text-gray-700" onclick="navigateToPage('career')">
+                Berbinar Careers
+            </div>
+            <div class="m-auto flex items-center justify-center rounded-md p-4 w-44 duration-300 cursor-pointer bg-primary hover:bg-sky-950 text-white" onclick="navigateToPage('work-with-us')">
+                <span class="text-lg">Work With Us</span>
+            </div>
+        </div>
+
+        <script type="text/javascript">
+            function Open() {
+                document.querySelector('.sidebar').classList.toggle('right-full');
+            }
+
+            function navigateToPage(page) {
+                switch (page) {
+                    case 'home':
+                        window.location.href = '/';
+                        break;
+                    case 'about-us':
+                        window.location.href = '/about-us';
+                        break;
+                    case 'products':
+                        window.location.href = '/products';
+                        break;
+                    case 'contact':
+                        window.location.href = '/contact';
+                        break;
+                    case 'career':
+                        window.location.href = '/careers';
+                        break;
+                    case 'work-with-us':
+                        window.location.href = '/work-with-us';
+                        break;
+                }
+            }
+        </script>
     </nav>
 </header>
