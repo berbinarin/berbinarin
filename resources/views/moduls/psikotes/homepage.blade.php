34823cb39e9a7887c8a7dc78df72f494ab0e9b51
KM6smpmaarif6po <km6.smpmaarif6po@gmail.com>
2024-03-07 18:55:53 +0700

    (pembuatan pop up notifikasi pada halaman psikotest)

--- resources/views/moduls/psikotes/homepage.blade.php
+++ resources/views/moduls/psikotes/homepage.blade.php
@@ -4,9 +4,25 @@
 ])
 
 @section('content')
-
-    <section
-        class="max-w-6xl mx-auto h-screen flex flex-col gap-8 md:flex-row items-center justify-center relative px-5 md:px-0 mt-12 md:mt-0">
+<style>
+#notificationPopup {
+    background-color: #2686AE;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 40;
+    padding: 30px 60px; /* Meningkatkan padding */
+    width: 90%; /* Menambahkan lebar */
+    max-width: 95vw; /* Batasi lebar maksimum */
+    max-height: 95%; /* Sesuaikan tinggi maksimum sesuai kebutuhan */
+    overflow-y: hidden; /* Menyembunyikan scrollbar vertikal */
+    text-align: center; /* Pusatkan konten */
+    font-size: 16px; /* Mengurangi ukuran teks */
+    border-radius: 20px; /* Menambahkan sudut yang lebih bulat */
+}
+</style>
+    <section class="max-w-6xl mx-auto h-screen flex flex-col gap-8 md:flex-row items-center justify-center relative px-5 md:px-0 mt-12 md:mt-0">
         {{-- HERO IMG MOBILE --}}
         <img src="{{ asset('assets/images/ilustrasi-logo-berbinar.png') }}" alt="Ilustrasi Logo Berbinar"
             title="Ilustrasi Logo Berbinar" class="w-full block md:hidden" data-aos="fade-up" data-aos-duration="1500">
@@ -22,7 +38,7 @@
                 <p class="text-lg text-disabled">Berbinar hadir untuk kamu yang ingin meningkatkan potensi diri melalui layanan tes psikotes individu dan perusahaan.</p>
                 <div class="flex gap-4">
                     <button
-                        class="text-lg text-[#70787D] bg-[#C1C1C1] rounded-md hover:bg-primary hover:text-white duration-700 px-5 py-2 w-fit showModal">Ikuti Tes Gratis</button>
+                        class="text-lg text-[#70787D] bg-[#C1C1C1] rounded-md hover:bg-primary hover:text-white duration-700 px-5 py-2 w-fit showmodalTes">Ikuti Tes Gratis</button>
                     <a href=""
                         class="text-lg text-white bg-primary rounded-md hover:bg-primary duration-700 px-5 py-2 w-fit">Daftar Tes Berbayar</a>
                 </div>
@@ -52,9 +68,9 @@
 
     {{-- POP UP --}}
     <section>
-        <div class="modal h-screen w-full fixed left-0 top-0 flex justify-center items-center bg-black bg-opacity-50 z-40 hidden">
+        <div class="modalTes h-screen w-full fixed left-0 top-0 flex justify-center items-center bg-black bg-opacity-50 z-40 hidden">
             <div class="bg-white rounded-xl shadow-lg w-[560px]">
-                <div class="text-right p-3 closeModal">
+                <div class="text-right p-3 closemodalTes">
                     <i class='bx bxs-x-circle text-[48px] text-[#F34949]'></i>
                 </div>
                 <div class="text-center">
@@ -65,18 +81,74 @@
                     <textarea name="alasan" placeholder="Alasan Mengikuti Tes" cols="30" rows="10" class="items-center justify-center rounded-md bg-[#F3F4F6] text-[#9B9B9B] w-[480px] h-60 p-4 my-1"></textarea>
                 </div>
                 <div class="flex justify-center items-center w-100 p-3 my-4 ">
-                    <a href="{{ route('psikotestFreeStart') }}"
-                        class="text-lg text-white bg-green-500 rounded-md hover:bg-primary duration-700 px-5 py-2 w-fit">Simpan</a>
+                    <button class="text-lg text-white bg-green-500 rounded-md hover:bg-primary duration-700 px-5 py-2 w-fit" id="saveButton">Simpan</button>
                 </div>
             </div>
         </div>
     </section>
 
-    <script>
-        const modal = document.querySelector('.modal');
 
+    <div id="notificationPopup" class="hidden">
+            <h2 class="intro_title text-center font-medium text-6xl m-12 leading-tight text-black">Penjelasan Psikotes</h2>
+            <p class="intro_description font-normal m-4 text-justify text-black">Apa itu Tes Kepribadian Big 5 OCEAN? <br>
+            Tes ini merupakan model dari tes lima dimensi kepribadian yang dapat mengungkapkan potensi karir yang sesuai dengan kepribadian Anda. Tes Lima Dimensi Kepribadian secara luas dianggap sebagai cara paling kuat untuk menggambarkan perbedaan kepribadian yang Anda miliki.
+            <br><br> Tes Lima Dimensi Kepribadian adalah salah satu alat tes untuk mengungkap kepribadian berdasarkan teori Big Five Personality yang dikemukakan oleh seorang psikolog terkenal, yaitu Lewis Goldberg. Dalam teori sifat kepribadian "The Big Five Personality Traits Model" tersebut terdiri dari lima dimensi kunci diantaranya seperti, Openness (O), Conscientiousness (C), Extraversion (E), Agreeableness (A) dan Neuroticism (N).
+        <br>Ikuti kuis ini untuk mengeksplorasi kepribadian Anda dengan Tes Lima Kepribadian, Anda akan melihat bagaimana lima dimensi kepribadian utama: Openness (O), Conscientiousness (C), Extraversion (E), Agreeableness (A) dan Neuroticism (N).</p>    
+            <div class="flex justify-start gap-4 my-12">
+                <button class="text-lg text-white bg-green-500 rounded-md hover:bg-white hover:text-primary duration-700 px-6 py-2 w-fit showModal">
+                    Mulai Tes
+                </button>
+                <a href="{{ route('psikotestHome') }}" class="text-white font-medium rounded-md border-2 border-white hover:bg-white hover:text-primary hover:cursor-pointer px-6 py-2 w-fit">
+                    Kembali ke Beranda
+                </a>
+            </div>
+    </div>    
+
+    <div class="modal h-screen w-full fixed left-0 top-0 flex justify-center items-center bg-black bg-opacity-50 z-50 hidden">
+        <div class="bg-white rounded-xl shadow-lg w-[560px]">
+            <div class="text-right p-3 closeModal">
+                <i class='bx bxs-x-circle text-[48px] text-[#F34949]'></i>
+            </div>
+            <div>
+                <div class="text-center">
+                    <h1 class="text-primary text-center text-3xl mb-4 -mt-10">Instruksi Pengisian</h1>
+                    <p class="font-bold px-24">Perhatikan dengan seksama instruksi pengisian berikut untuk mengisi free tes psikotes berbinar</p>
+                </div>
+                <div class="px-16 py-10">
+                    <p class="text-justify">Pada tes ini, setiap nomor berisikan satu pernyataan beserta lima pilihan skor jawaban. Tugas Anda adalah menentukan <span class="font-bold">skor kesesuaian</span> setiap pernyataan dengan keadaan diri Kamu yang sebenarnya. Tiap pilihan skor kesesuaian yang Anda pilih memiliki kriterianya masing-masing.</p>
+                    <p class="font-bold"><br>Keterangan Skor:</p>
+                    <p>1: Sangat tidak sesuai</p>
+                    <p>2: Tidak sesuai</p>
+                    <p>3: Ragu-ragu</p>
+                    <p>4: Sesuai</p>
+                    <p>5: Sangat sesuai</p>
+                </div>
+            </div>
+            <div class="flex justify-center items-center w-100 p-3">
+                <a href="{{ route('psikotestFreeTest') }}"
+                    class="text-lg text-white bg-green-500 rounded-md hover:bg-primary duration-700 px-8 mt-0 mb-4 py-2 w-fit">Mulai</a>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        const modalTes = document.querySelector('.modalTes');
+        const notificationPopup = document.getElementById('notificationPopup');
+        const modal = document.querySelector('.modal');
+        const showmodalTes = document.querySelector('.showmodalTes');
+        const closemodalTes = document.querySelector('.closemodalTes');
+        const closeNotificationBtn = document.getElementById('closeNotification');
         const showModal = document.querySelector('.showModal');
         const closeModal = document.querySelector('.closeModal');
+        const saveButton = document.getElementById('saveButton');
+
+        showmodalTes.addEventListener('click', function(){
+            modalTes.classList.remove('hidden')
+        });
+
+        closemodalTes.addEventListener('click', function(){
+            modalTes.classList.add('hidden')
+        });
 
         showModal.addEventListener('click', function(){
             modal.classList.remove('hidden')
@@ -84,6 +156,15 @@
 
         closeModal.addEventListener('click', function(){
             modal.classList.add('hidden')
+        });
+
+        saveButton.addEventListener('click', function(){
+            // Tampilkan pop-up notifikasi
+            notificationPopup.classList.remove('hidden');
+        });
+
+        closeNotificationBtn.addEventListener('click', function() {
+            notificationPopup.classList.add('hidden');
         });
     </script>
     
