2999804dbe60cf390e49c781c9aecac5a815caf0
Idooo <ridhoaditya925@gmail.com>
2024-06-15 20:59:00 +0800

    fix: edit path

--- app/Http/Middleware/AuthenticateBerbinarPlus.php
+++ app/Http/Middleware/AuthenticateBerbinarPlus.php
@@ -5,7 +5,7 @@ namespace App\Http\Middleware;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
 
-class AuthenticateBerbinarPLus extends Middleware
+class AuthenticateBerbinarPlus extends Middleware
 {
     /**
      * Get the path the user should be redirected to when they are not authenticated.
