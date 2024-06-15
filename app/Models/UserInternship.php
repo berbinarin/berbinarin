d35cee2781993ce54c6e6d3a95ca653d50b80b8d
Idooo <ridhoaditya925@gmail.com>
2024-06-16 00:50:13 +0800

    feat: add userinternship model

---
+++ app/Models/UserInternship.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserInternship extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'email', 'nama_lengkap', 'nama_panggilan', 'tanggal_lahir',
+        'no_whatsapp', 'asal_kota', 'asal_pendidikan', 'status_kelas', 'jurusan',
+        'akun_instagram', 'akun_tiktok', 'akun_linkdin', 'sumber_informasi',
+        'tautan_cv', 'tautan_portofolio', 'tautan_berkas_ss', 'motivasi'
+    ];
+}
