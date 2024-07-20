d291463058c007383a51c6ea670efd49c0f32f33
idoo0 <ridhoaditya925@gmail.com>
2024-07-20 18:21:41 +0800

    fix: change extend model

--- app/Models/PsikotestPaid/UserPsikotestPaid.php
+++ app/Models/PsikotestPaid/UserPsikotestPaid.php
@@ -4,8 +4,9 @@ namespace App\Models\PsikotestPaid;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class UserPsikotestPaid extends Model
+class UserPsikotestPaid extends Authenticatable
 {
     use HasFactory;
 
