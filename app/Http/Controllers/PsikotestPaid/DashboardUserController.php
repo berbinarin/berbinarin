3f44a4f80de432d45e3ee15feb793ea60899a2da
adamwisnup <wisnuadam21@gmail.com>
2024-08-09 23:06:44 +0700

    chore: Add status_progress field to PsikotestPaidTest model and update PsikotestPaid dashboard routes

--- app/Http/Controllers/PsikotestPaid/DashboardUserController.php
+++ app/Http/Controllers/PsikotestPaid/DashboardUserController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers\PsikotestPaid;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\PsikotestPaid\PsikotestTool;
 use App\Models\PsikotestPaid\PsikotestType;
 use App\Models\PsikotestPaid\UserPsikotestPaid;
 
 class DashboardUserController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['login']);
+        $this->middleware('role:Admin,HR,Konselling,PsikotestFree,psikotestpaid')->except(['login']);
+    }
+
     public function userRegister()
     {
         $users = UserPsikotestPaid::all();
@@ -23,31 +31,56 @@ class DashboardUserController extends Controller
         return view('moduls.psikotes-paid.dashboard.user-detail', compact('user'));
     }
 
+    public function psikotestTool()
+    {
+        $tools = PsikotestTool::all();
+
+        return view('moduls.psikotes-paid.dashboard.psikotest-tool', compact('tools'));
+    }
+
+    public function generateToken($id)
+    {
+        $tool = PsikotestTool::findOrFail($id);
+        $tool->token = Str::random(8);
+        $tool->save();
+
+        return redirect()->back()->with('success', 'Token generated successfully!');
+    }
+
+    public function deleteToken($id)
+    {
+        $tool = PsikotestTool::findOrFail($id);
+        $tool->token = null;
+        $tool->save();
+
+        return redirect()->back()->with('success', 'Token deleted successfully!');
+    }
+
     public function priceListIndividu()
     {
         $psikotestIndividu = PsikotestType::where('category_psikotest_type_id', 2)->get();
 
-        return view('moduls.psikotes-paid.dashboard.price-list-individu', compact('psikotestIndividu'));
+        return view('moduls.dashboard.psikotes-paid.individu', compact('psikotestIndividu'));
     }
 
     public function priceListCommunity()
     {
         $psikotestCommunity = PsikotestType::where('category_psikotest_type_id', 1)->get();
 
-        return view('moduls.psikotes-paid.dashboard.price-list-community', compact('psikotestCommunity'));
+        return view('moduls.dashboard.psikotes-paid.komunitas', compact('psikotestCommunity'));
     }
 
     public function priceListCorporate()
     {
         $psikotestCorporate = PsikotestType::where('category_psikotest_type_id', 4)->get();
 
-        return view('moduls.psikotes-paid.dashboard.price-list-corporate', compact('psikotestCorporate'));
+        return view('moduls.dashboard.psikotes-paid.perusahaan', compact('psikotestCorporate'));
     }
 
     public function priceListEducationalInstitution()
     {
         $psikotestSchool = PsikotestType::where('category_psikotest_type_id', 3)->get();
 
-        return view('moduls.psikotes-paid.dashboard.price-list-school', compact('psikotestSchool'));
+        return view('moduls.dashboard.psikotes-paid.pendidikan', compact('psikotestSchool'));
     }
 }
