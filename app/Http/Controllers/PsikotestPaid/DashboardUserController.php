99d12129f4d634c566f893146b54e2003d8f0f2e
adamwisnup <wisnuadam21@gmail.com>
2024-08-09 21:03:54 +0700

    feat: Add price list pages for different categories

--- app/Http/Controllers/PsikotestPaid/DashboardUserController.php
+++ app/Http/Controllers/PsikotestPaid/DashboardUserController.php
@@ -4,13 +4,13 @@ namespace App\Http\Controllers\PsikotestPaid;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\PsikotestPaid\PsikotestType;
 use App\Models\PsikotestPaid\UserPsikotestPaid;
 
 class DashboardUserController extends Controller
 {
     public function userRegister()
     {
-        // getAll user yang sudah register
         $users = UserPsikotestPaid::all();
 
         return view('moduls.psikotes-paid.dashboard.user-register', compact('users'));
@@ -18,9 +18,36 @@ class DashboardUserController extends Controller
 
     public function userDetail($id)
     {
-        // get user by id
         $user = UserPsikotestPaid::find($id);
 
         return view('moduls.psikotes-paid.dashboard.user-detail', compact('user'));
+    }
+
+    public function priceListIndividu()
+    {
+        $psikotestIndividu = PsikotestType::where('category_psikotest_type_id', 2)->get();
+
+        return view('moduls.psikotes-paid.dashboard.price-list-individu', compact('psikotestIndividu'));
+    }
+
+    public function priceListCommunity()
+    {
+        $psikotestCommunity = PsikotestType::where('category_psikotest_type_id', 1)->get();
+
+        return view('moduls.psikotes-paid.dashboard.price-list-community', compact('psikotestCommunity'));
+    }
+
+    public function priceListCorporate()
+    {
+        $psikotestCorporate = PsikotestType::where('category_psikotest_type_id', 4)->get();
+
+        return view('moduls.psikotes-paid.dashboard.price-list-corporate', compact('psikotestCorporate'));
+    }
+
+    public function priceListEducationalInstitution()
+    {
+        $psikotestSchool = PsikotestType::where('category_psikotest_type_id', 3)->get();
+
+        return view('moduls.psikotes-paid.dashboard.price-list-school', compact('psikotestSchool'));
     }
 }
