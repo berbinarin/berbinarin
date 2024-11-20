1f827731850a64eedfc6cc617da36900b2b307d6
latief55 <latief0505050@gmail.com>
2024-11-20 22:25:39 +0700

    feat: Dashboard Biodata, Sidebar, Route in data-test.php

--- resources/views/moduls/dashboard/psikotes-paid/tools/biodata/individual.blade.php
+++ resources/views/moduls/dashboard/psikotes-paid/tools/biodata/individual.blade.php
@@ -35,33 +35,14 @@
                                 <td class="text-center">CEO</td>
                                 <td class="text-center">18-04-2024</td>
                                 <td class="px-6 py-4 whitespace-no-wrap text-center text-center">
-                                    <button onclick="toggleModal('view-modal')" type="button"
+                                    <a href="{{ route('dashboard.psikotespaid.tableindividual') }}">
+                                    <button type="button"
                                         class="focus:ring-2 focus:ring-offset-2  mt-4 sm:mt-0 inline-flex items-start justify-start p-3 bg-blue-500 hover:bg-blue-500 focus:outline-none rounded">
                                         <p class="font-medium leading-none text-white">Lihat Jawaban</p>
                                     </button>
+                                    </a>
                                 </td>
                             </tr>
-                            {{-- modal delete job description --}}
-                            <div class="hidden overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center" id="view-modal">
-                                <div class="relative w-auto my-6 mx-auto max-w-3xl">
-                                    <!--content-->
-                                    <div class="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                                        <div class="relative p-6 flex-auto">
-                                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuaPhxgtyaCQStpttEXIZj4VctG9goIoOAtQ&s" alt="">
-                                                <!--footer-->
-                                                <div class="flex items-center justify-end pt-3 border-t border-solid border-blueGray-200 rounded-b">
-                                                        <button class="focus:ring-2 focus:ring-offset-2  mt-4 sm:mt-0 inline-flex items-start justify-start p-3 focus:outline-none rounded" 
-                                                        type="button" onclick="toggleModal('view-modal')" style="background-color: #6083F2;">
-                                                        <p class="font-medium leading-none text-white">Kembali</p>
-                                                    </button>
-                                                </div>
-                                            </form>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            
-                            <div class="hidden opacity-25 fixed inset-0 z-40 bg-black" id="view-modal-backdrop"></div>
                         </tbody>
                     </table>
                 </div>
@@ -69,13 +50,4 @@
         </div>
     </div>
 </section>
-
-<script type="text/javascript">
-    function toggleModal(modalID) {
-        document.getElementById(modalID).classList.toggle("hidden");
-        document.getElementById(modalID + "-backdrop").classList.toggle("hidden");
-        document.getElementById(modalID).classList.toggle("flex");
-        document.getElementById(modalID + "-backdrop").classList.toggle("flex");
-    }
-</script>
 @endsection
