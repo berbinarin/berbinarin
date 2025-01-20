81534e0b1c9de3403f18af6c0ffb9d34a4ff5e95
Muhammad <muhammadahjahh@gmail.com>
2025-01-20 21:28:43 +0800

    feat: add berbinarFamily page

--- resources/views/moduls/dashboard/hr/berbinar-family/addBerbinarFamily.blade.php
+++ resources/views/moduls/dashboard/hr/berbinar-family/addBerbinarFamily.blade.php
@@ -54,31 +54,77 @@
                     <div class="w-1/4"></div>
                 </form>
                 <h1 class="text-2xl font-bold pt-10">Riwayat Jabatan</h1>
-                <form class="pt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 w-full">
-                    {{-- focus waarna birunya masih ada --}}
-                    <div class="flex flex-col gap-2 w-full">
-                        <p class="text-lg font-semibold flex gap-1">
-                            Nama Lengkap<i class="bx bxs-star text-xs text-red-600 "></i></p>
-                        <input type="text" name="name"
-                            class="py-2 px-3 rounded-lg border-gray-300 focus:outline-none focus:border-transparent focus:ring-0 shadow-sm "
-                            placeholder="John Doe" />
-                    </div>
-                    <div class="flex flex-col gap-2 w-full">
-                        <p class="text-lg font-semibold flex gap-1">
-                            LinkedIn<i class="bx bxs-star text-xs text-red-600 "></i></p>
-                        <input type="text" name="name"
-                            class="py-2 px-3 rounded-lg border-gray-300 focus:outline-none focus:border-transparent focus:ring-0 shadow-sm "
-                            placeholder="linkedin.com/in/johndoe/" />
-                    </div>
-                    <div class="w-1/4"></div>
-                    <div class="flex flex-col gap-2 w-full">
-                        <p class="text-lg font-semibold flex gap-1">
-                            LinkedIn<i class="bx bxs-star text-xs text-red-600 "></i></p>
-                        <input type="text" name="name"
-                            class="py-2 px-3 rounded-lg border-gray-300 focus:outline-none focus:border-transparent focus:ring-0 shadow-sm "
-                            placeholder="linkedin.com/in/johndoe/" />
-                    </div>
-                </form>
+                <div class="flex pb-5">
+                    <form class="pt-5 grid grid-cols-2 lg:grid-cols-2 gap-x-10 gap-y-5 w-full">
+                        {{-- focus waarna birunya masih ada --}}
+                        <div class="flex flex-col gap-2 w-full">
+                            <p class="text-lg font-semibold flex gap-1">
+                                Posisi<i class="bx bxs-star text-xs text-red-600"></i>
+                            </p>
+                            <select name="position" 
+                                class="py-2 px-3 rounded-lg border-gray-300 focus:outline-none focus:border-blue-500 shadow-sm text-gray-500">
+                                <option value="" disabled selected class="text-black">Pilih posisi jabatan</option>
+                                <option value="frontend" class="text-black">Frontend</option>
+                                <option value="backend" class="text-black">Backend</option>
+                                <option value="fullstack" class="text-black">Fullstack</option>
+                            </select>
+                        </div>
+                        <div class="flex flex-col gap-2 w-full">
+                            <p class="text-lg font-semibold flex gap-1">
+                                Divisi<i class="bx bxs-star text-xs text-red-600"></i>
+                            </p>
+                            <select name="position" 
+                                class="py-2 px-3 rounded-lg border-gray-300 focus:outline-none focus:border-blue-500 shadow-sm text-gray-500">
+                                <option value="" disabled selected class="text-black">Pilih divisi</option>
+                                <option value="frontend" class="text-black">Frontend</option>
+                                <option value="backend" class="text-black">Backend</option>
+                                <option value="fullstack" class="text-black">Fullstack</option>
+                            </select>
+                        </div>
+                        <div class="flex flex-col gap-2 w-full">
+                            <p class="text-lg font-semibold flex gap-1">
+                                Sub divisi<i class="bx bxs-star text-xs text-red-600"></i>
+                            </p>
+                            <select name="position" 
+                                class="py-2 px-3 rounded-lg border-gray-300 focus:outline-none focus:border-blue-500 shadow-sm text-gray-500">
+                                <option value="" disabled selected class="text-black">Pilih subdivisi</option>
+                                <option value="frontend" class="text-black">Frontend</option>
+                                <option value="backend" class="text-black">Backend</option>
+                                <option value="fullstack" class="text-black">Fullstack</option>
+                            </select>
+                        </div>
+                        <div class="flex flex-col gap-2 w-full">
+                            <p class="text-lg font-semibold flex gap-1">
+                                Awal Menjabat<i class="bx bxs-star text-xs text-red-600"></i>
+                            </p>
+                            <div class=" rounded-lg border border-gray-300 focus:outline-none focus:border-transparent focus:ring-0 shadow-sm flex justify-between">
+                                <input type="date" class="border-none rounded-lg text-gray-500 w-full"/>
+                            </div>
+                        </div>
+                        <div class="flex flex-col gap-2 w-full">
+                            <p class="text-lg font-semibold flex gap-1">
+                                Akhir Menjabat<i class="bx bxs-star text-xs text-red-600"></i>
+                            </p>
+                            <div class=" rounded-lg border border-gray-300 focus:outline-none focus:border-transparent focus:ring-0 shadow-sm flex justify-between">
+                                <input type="date" class="border-none rounded-lg text-gray-500 w-full"/>
+                            </div>
+                        </div>
+                    </form>
+                    <div class="w-1/6"></div>
+                </div>
+                <div class="py-2 flex items-center justify-center border border-dashed rounded-lg border-blue-500 text-blue-500">
+                    <h1 class="">
+                        <i class="bx bx-plus-circle"></i>
+                        Tambahkan Riwayat Jabatan
+                    </h1>
+                </div>
+                <div class="bg-gray-300 h-1 mt-8"></div>
+                <div class="flex justify-end pt-5">
+                    <button
+                    class="flex items-center gap-2 bg-blue-500 text-white rounded-xl px-3 py-2 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300">
+                    Unggah Data
+                </button>
+                </div>
             </div>
         </div>
     </div>
