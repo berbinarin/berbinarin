40631961fb5571ea3ddf77b0c7da6b338a93ca88
Muhammad <muhammadahjahh@gmail.com>
2025-02-01 15:26:58 +0700

    .

--- app/Http/Controllers/KeluargaBerbinar/DataStaffController.php
+++ app/Http/Controllers/KeluargaBerbinar/DataStaffController.php
@@ -24,6 +24,7 @@ class DataStaffController extends Controller
         $validated = $request->validate([
             'nama_lengkap' => 'required|string|min:5',
             'linkedin' => 'required|string',
+            'foto' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
             'motm' => 'nullable|in:yes,no',
         ]);
         $validated['motm'] = $validated['motm'] ?? 'no';
