10fce16ad7bf4be7c62290616cf6de6072c96e02
MasKanz <kanzalkautsar@gmail.com>
2025-08-18 22:11:16 +0700

    fix : memperbaiki menu Kode Voucher

--- resources/views/dashboard/partials/sidebar/counseling-pm.blade.php
+++ resources/views/dashboard/partials/sidebar/counseling-pm.blade.php
@@ -50,9 +50,9 @@
     </a>
 </li> --}}
 
-<li class="dark-hover:text-blue-300 my-5 rounded-lg p-2">
-    <a href="{{ route("dashboard.code-voucher.index") }}" class="{{ isRouteNameStartWith("dashboard.code-voucher") }} flex flex-row items-center text-gray-700 duration-700 hover:text-primary">
-        <i class="bx bx-credit-card {{ isRouteNameStartWith("dashboard.code-voucher") }} mr-2 text-lg text-gray-700"></i>
+<li class="dark-hover:text-blue-300 my-5 rounded-lg p-2 {{ isRouteNameStartWith('dashboard.code-voucher', 'bg') }}">
+    <a href="{{ route('dashboard.code-voucher.index') }}" class="flex flex-row items-center duration-700 {{ isRouteNameStartWith('dashboard.code-voucher') }}">
+        <i class="bx bx-credit-card mr-2 text-lg {{ Str::startsWith(Route::currentRouteName(), 'dashboard.code-voucher') ? 'text-white' : 'text-gray-700' }}"></i>
         <span class="ml-4 text-base font-bold leading-5">Kode Voucher</span>
     </a>
 </li>
