eed53618d4d48b80af75c2419edd7b72e3aa9623
MasKanz <kanzalkautsar@gmail.com>
2025-08-18 23:54:59 +0700

    fix : mengurangi link menu sidebar counseling-pm

--- resources/views/dashboard/partials/sidebar/counseling-pm.blade.php
+++ resources/views/dashboard/partials/sidebar/counseling-pm.blade.php
@@ -29,37 +29,9 @@
     </a>
 </li>
 
-{{-- <li class="dark-hover:text-blue-300 my-5 rounded-lg p-2 {{ isRouteNameStartWith('dashboard.peer-counselors', 'bg') }}">
-    <a href="{{ route('dashboard.peer-counselors.index') }}" class="flex flex-row items-center duration-700 {{ isRouteNameStartWith('dashboard.peer-counselors') }}">
-        <i class="bx bx-user mr-2 text-lg {{ Str::startsWith(Route::currentRouteName(), 'dashboard.peer-counselors') ? 'text-white' : 'text-gray-700' }}"></i>
-        <span class="ml-4 text-base font-bold leading-5">Peer Counselor Data</span>
-    </a>
-</li>
-
-<li class="dark-hover:text-blue-300 my-5 rounded-lg p-2 {{ isRouteNameStartWith('dashboard.psychologists', 'bg') }}">
-    <a href="{{ route('dashboard.psychologists.index') }}" class="flex flex-row items-center duration-700 {{ isRouteNameStartWith('dashboard.psychologists') }}">
-        <i class="bx bx-user mr-2 text-lg {{ Str::startsWith(Route::currentRouteName(), 'dashboard.psychologists') ? 'text-white' : 'text-gray-700' }}"></i>
-        <span class="ml-4 text-base font-bold leading-5">Psikolog Data</span>
-    </a>
-</li>
-
-<li class="dark-hover:text-blue-300 my-5 rounded-lg p-2 {{ isRouteNameStartWith('dashboard.berbinar-for-u', 'bg') }}">
-    <a href="{{ route('dashboard.berbinar-for-u.index') }}" class="flex flex-row items-center duration-700 {{ isRouteNameStartWith('dashboard.berbinar-for-u') }}">
-        <i class="bx bx-user mr-2 text-lg {{ Str::startsWith(Route::currentRouteName(), 'dashboard.berbinar-for-u') ? 'text-white' : 'text-gray-700' }}"></i>
-        <span class="ml-4 text-base font-bold leading-5">Berbinar For U Data</span>
-    </a>
-</li> --}}
-
 <li class="dark-hover:text-blue-300 my-5 rounded-lg p-2 {{ isRouteNameStartWith('dashboard.code-voucher', 'bg') }}">
     <a href="{{ route('dashboard.code-voucher.index') }}" class="flex flex-row items-center duration-700 {{ isRouteNameStartWith('dashboard.code-voucher') }}">
         <i class="bx bx-credit-card mr-2 text-lg {{ Str::startsWith(Route::currentRouteName(), 'dashboard.code-voucher') ? 'text-white' : 'text-gray-700' }}"></i>
         <span class="ml-4 text-base font-bold leading-5">Kode Voucher</span>
     </a>
 </li>
-
-{{-- <li class="dark-hover:text-blue-300 my-5 rounded-lg p-2 {{ isRouteNameStartWith('dashboard.psikolog-staff', 'bg') }}">
-    <a href="{{ route('dashboard.psikolog-staff.index') }}" class="flex flex-row items-center duration-700 {{ isRouteNameStartWith('dashboard.psikolog-staff') }}">
-        <i class="bx bx-table mr-2 text-lg {{ Str::startsWith(Route::currentRouteName(), 'dashboard.psikolog-staff') ? 'text-white' : 'text-gray-700' }}"></i>
-        <span class="ml-4 text-base font-bold leading-5">Psikolog Staff</span>
-    </a>
-</li> --}}
