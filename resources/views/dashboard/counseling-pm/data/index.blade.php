c7a6e8b9b9ba4402a2791e0fc1d538ecdcf002eb
citrahlzh <citrahalijah1992@gmail.com>
2025-08-21 21:14:32 +0700

    fix: uploading image in counseling

--- resources/views/dashboard/counseling-pm/data/index.blade.php
+++ resources/views/dashboard/counseling-pm/data/index.blade.php
@@ -17,7 +17,7 @@
 
                             <a href="{{ route('dashboard.peer-counselors.index') }}" class="flex flex-row items-center rounded-xl bg-white p-6 shadow hover:bg-gray-100 transition">
                                 <div class="flex h-[104px] w-[119px] items-center justify-center rounded-lg bg-[#3986A31A]/10">
-                                    <img src="{{ asset("assets/images/landing/asset-konseling/vector/handshake.png") }}" alt="Peer Counselor">
+                                    <img src="{{ asset("assets/images/landing/asset-konseling/image/handshake.png") }}" alt="Peer Counselor">
                                 </div>
                                 <div class="ml-6">
                                     <span class="text-[28px] font-semibold text-gray-800">Peer Counselor Data</span>
@@ -26,7 +26,7 @@
 
                             <a href="{{ route('dashboard.psychologists.index') }}" class="flex flex-row items-center rounded-xl bg-white p-6 shadow hover:bg-gray-100 transition">
                                 <div class="flex h-[104px] w-[119px] items-center justify-center rounded-lg bg-[#3986A31A]/10">
-                                    <img src="{{ asset("assets/images/landing/asset-konseling/vector/psychology.png") }}" alt="Psikolog">
+                                    <img src="{{ asset("assets/images/landing/asset-konseling/image/psychology.png") }}" alt="Psikolog">
                                 </div>
                                 <div class="ml-6">
                                     <span class="text-[28px] font-semibold text-gray-800">Psikolog Data</span>
@@ -35,7 +35,7 @@
 
                             <a href="{{ route('dashboard.berbinar-for-u.index') }}" class="flex flex-row items-center rounded-xl bg-white p-6 shadow hover:bg-gray-100 transition">
                                 <div class="flex h-[104px] w-[119px] items-center justify-center rounded-lg bg-[#3986A31A]/10">
-                                    <img src="{{ asset("assets/images/landing/asset-konseling/vector/hand-heart.png") }}" alt="Berbinar for U">
+                                    <img src="{{ asset("assets/images/landing/asset-konseling/image/hand-heart.png") }}" alt="Berbinar for U">
                                 </div>
                                 <div class="ml-6">
                                     <span class="text-[28px] font-semibold text-gray-800">Berbinar for U Data</span>
